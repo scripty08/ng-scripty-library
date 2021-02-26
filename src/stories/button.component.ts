8bd0d10ab98b3d1fd8a20490691833d73c39b2f9
scripty <scripty@outlook.de>
2021-02-26 06:10:17 +0100

    initial commit

---
+++ src/stories/button.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'storybook-button',
+  template: ` <button
+    type="button"
+    (click)="onClick.emit($event)"
+    [ngClass]="classes"
+    [ngStyle]="{ 'background-color': backgroundColor }"
+  >
+    {{ label }}
+  </button>`,
+  styleUrls: ['./button.css'],
+})
+export default class ButtonComponent {
+
+  @Input()
+  primary = false;
+
+  @Input()
+  backgroundColor?: string;
+
+
+  @Input()
+  size: 'small' | 'medium' | 'large' = 'medium';
+
+  @Input()
+  label = 'Button';
+
+  @Output()
+  onClick = new EventEmitter<Event>();
+
+  public get classes(): string[] {
+    const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    return ['storybook-button', `storybook-button--${this.size}`, mode];
+  }
+}
